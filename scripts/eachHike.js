469b15fc7c414c26ff755f15b6996c3eec4443fc
Davin Leong <davinleongg@gmail.com>
2023-02-28 10:24:25 -0800

    all updates for the demo09

---
+++ scripts/eachHike.js
@@ -0,0 +1,21 @@
+function displayHikeInformation() {
+  //retrieve the document id from the url
+  let params = new URL(window.location.href) //get the url from the searchbar
+  let ID = params.searchParams.get("docID");
+  console.log(ID);
+
+  db.collection("hikes").doc(ID).get().then(thisHike => {
+    hikeInfo = thisHike.data();
+    hikeCode = hikeInfo.code;
+    hikeName = hikeInfo.name;
+
+    document.getElementById("hikeName").innerHTML = hikeName;
+    let imgEvent = document.querySelector(".hike-img");
+    imgEvent.src = "../images/" + hikeCode + ".jpg";
+
+  }
+  )
+
+
+}
+displayHikeInformation();
